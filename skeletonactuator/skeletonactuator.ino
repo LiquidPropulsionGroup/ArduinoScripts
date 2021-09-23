3d859c8a2755adddfe5dbda007c1335fceaf61a7
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2021-09-23 15:41:37 -0700

    Example code for gating messages based on whether states have changed

--- skeletonactuator/skeletonactuator.ino
+++ skeletonactuator/skeletonactuator.ino
@@ -30,6 +30,18 @@ OneByte LOX_Purge;
 
 FourBytes Terminator;
 
+// Global variables for storing previous state values
+bool FUEL_Press_Prev;
+bool LOX_Press_Prev;
+bool FUEL_Vent_Prev;
+bool LOX_Vent_Prev;
+bool MAIN_Prev;
+bool FUEL_Purge_Prev;
+bool LOX_Purge_Prev;
+bool STATE_CHANGED;
+
+int count = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -46,34 +58,58 @@ void setup() {
   MAIN.state = false;                 // NORMALLY CLOSED
   FUEL_Purge.state = false;           // NORMALLY CLOSED
   LOX_Purge.state = false;            // NORMALLY CLOSED
-  
 
+  // Initialize the previous <open/closed> state information
+  FUEL_Press_Prev = FUEL_Press.state;
+  LOX_Press_Prev = LOX_Press.state;
+  FUEL_Vent_Prev = FUEL_Vent.state;
+  LOX_Vent_Prev = LOX_Vent.state;
+  MAIN_Prev = MAIN.state;
+  FUEL_Purge_Prev = FUEL_Purge.state;
+  LOX_Purge_Prev = LOX_Purge.state;
+  STATE_CHANGED = false;
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   TimeStamp.int_dat = millis();
 
-  // Serial Writes
-  // Writing the Packet Start bytestring to the Serial Buffer
-  //Serial.write(Packet_Start.bytes,4);
-
-  // Writing the Timestamp to the Serial Buffer
-  //Serial.write(TimeStamp.bytes, 4);
   
-  // Writing the actuator states to the Serial Buffer
-  Serial.write(FUEL_Press.bytes, 1);
-  Serial.write(LOX_Press.bytes, 1);
-  Serial.write(FUEL_Vent.bytes, 1);
-  Serial.write(LOX_Vent.bytes, 1);
-  Serial.write(MAIN.bytes, 1);
-  Serial.write(FUEL_Purge.bytes, 1);
-  Serial.write(LOX_Purge.bytes, 1);
+  if (count >= 10) {
+    FUEL_Press.state = !FUEL_Press.state;
+    count = 0;
+    STATE_CHANGED = true;
+  }
 
-  // Writing the Packet Stop bytestring to the Serial Buffer
-  //Serial.write(Terminator.bytes, 4);
+  // Only write to serial if the states have changed
+  if (STATE_CHANGED) {
+    // Serial Writes
+    // Writing the Packet Start bytestring to the Serial Buffer
+    //Serial.write(Packet_Start.bytes,4);
+
+    // Writing the Timestamp to the Serial Buffer
+    //Serial.write(TimeStamp.bytes, 4);
+  
+    // Writing the actuator states to the Serial Buffer
+    Serial.write(FUEL_Press.bytes, 1);
+    Serial.write(LOX_Press.bytes, 1);
+    Serial.write(FUEL_Vent.bytes, 1);
+    Serial.write(LOX_Vent.bytes, 1);
+    Serial.write(MAIN.bytes, 1);
+    Serial.write(FUEL_Purge.bytes, 1);
+    Serial.write(LOX_Purge.bytes, 1);
+
+    // Writing the Packet Stop bytestring to the Serial Buffer
+    //Serial.write(Terminator.bytes, 4);
+    STATE_CHANGED = false;
+  }
+  
+  // Otherwise do nothing
+  Serial.print(count);
   Serial.write('\n');
-
+  count += 1;
   delay(1000);
 
 }

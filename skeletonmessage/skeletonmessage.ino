0cfacafc4573012fbe020fe08c45036f5c9869fb
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-03-06 01:23:29 -0800

    Match wires to boards to reading pins

--- skeletonmessage/skeletonmessage.ino
+++ skeletonmessage/skeletonmessage.ino
@@ -79,7 +79,7 @@ void setup() {
   Terminator.int_dat = pow(2,32)-1;  //4294967295;
 //  Label.int_dat = 0;
   memcpy(&SensorDataMessage[0], Packet_Start.bytes, 4);
-  memcpy(&SensorDataMessage[SENSOR_MESSAGE_LENGTH  - 4], Terminator.bytes, 4);
+  memcpy(&SensorDataMessage[SENSOR_MESSAGE_LENGTH - 4], Terminator.bytes, 4);
 
 //  Serial.println("PACKET INIT");
   // Initialize ADS1115
@@ -145,8 +145,8 @@ void loop() {
 //  Serial.print(PT_HE.int_dat, 4);
 
   // Uncalibrated, static for testing
-  PT_Purge.int_dat = 20;//random(0,500);
-  PT_Pneu.int_dat = 30;//random(0,500);
+  PT_Purge.int_dat = 0;//random(0,500);
+  PT_Pneu.int_dat = 0;//random(0,500);
 
   // Collect calibrated data from PT_FUEL_PV
   PT_FUEL_PV.int_dat = (adc48p1*0.000125)*442-258;
@@ -191,28 +191,28 @@ void loop() {
 //  Serial.println(PT_LOX_PV.int_dat);
 
   // Uncalibrated data, static for testing
-  PT_CHAM.int_dat = 100;//random(0,500);
+  PT_CHAM.int_dat = 0;//random(0,500);
 
   // Collect uncalibrated data from TC_FUEL_PV
-  TC_FUEL_PV.int_dat = ((adc4Ap0*0.000125)-1.25) * 200;
+  TC_FUEL_PV.int_dat = ((adc4Ap2*0.000125)-1.25) * 200;
 
   // Collect uncalibrated data from TC_LOX_PV
-  TC_LOX_PV.int_dat = ((adc4Ap1*0.000125)-1.25) * 200;
+  TC_LOX_PV.int_dat = ((adc4Ap3*0.000125)-1.25) * 200;
 
   // Uncalibrated data, static for testing
-  TC_LOX_Valve_Main.int_dat = 130;//random(0,500);
-  TC_WATER_In.int_dat = 140;//random(0,500);
-  TC_WATER_Out.int_dat = 150;//random(0,500);
-  TC_CHAM.int_dat = 300;//random(0,500);
+  TC_LOX_Valve_Main.int_dat = 0;//random(0,500);
+  TC_WATER_In.int_dat = 0;//random(0,500);
+  TC_WATER_Out.int_dat = 0;//random(0,500);
+  TC_CHAM.int_dat = 0;//random(0,500);
   //RC_LOX_Level.int_dat = random(0,500);
-  FT_Thrust.int_dat = 500;//random(0,500);
+  FT_Thrust.int_dat = 0;//random(0,500);
 
 // Debug
 //  Label.int_dat = Label.int_dat + 1;
 //  Serial.print("TC_FUEL_PV: ");
 //  Serial.print(TC_FUEL_PV.int_dat);
 //  Serial.print("  Steps: ");
-//  Serial.print(adc4Ap0);emp2;
+//  Serial.print(adc4Ap0);
 //  temp1 = analogRead(A0);
 //  Serial.print("TC1: ");
 //  Serial.print(temp1/1023.000*5, 4);
@@ -254,7 +254,6 @@ void loop() {
 //  Serial.println(" ");
 //  Serial.println("=============LOOP ENDED============");
 //  Serial.println(loop_dur);
-
 
   // Configurable delay
   delay(BUFFER_DELAY);

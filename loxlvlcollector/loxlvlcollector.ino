b594b1357bec53df743059f206d4f3abcbf17fb0
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2022-06-23 02:47:38 -0700

    Fractionalize the fill level output

--- loxlvlcollector/loxlvlcollector.ino
+++ loxlvlcollector/loxlvlcollector.ino
@@ -14,10 +14,14 @@ int pulse_pin = 8;
 uint32_t time_high;
 uint32_t time_low;
 uint32_t time_total;
-static double time_constant = log(2);       // ln(2)
-static int resistor_one = 500;              // 500 Ohm resistor
-static long resistor_two = 10000000;        // 10 MOhm resistor
-float capacitance;
+static double time_constant = log(2);                   // ln(2)
+static int resistor_one = 500;                          // 500 Ohm resistor
+static long resistor_two = 10000000;                    // 10 MOhm resistor
+double capacitance;                                     // measured capacitance
+static double EMPTY_CAPACITOR_CAPACITANCE = 5.02e-11;   // baseline air in capacitor
+double rel_capacitance;                                 // relative capacitance, fractional
+static double CAPACITOR_RANGE = 3.97e-9;
+float frac_capacitance;
 char str[255];
 
 // For profiling the code
@@ -26,20 +30,26 @@ long baseTime;
 
 // Union definitions for serialization
 typedef union FourBytes{
-  float floatDat;
+  double floatDat;
   int32_t numDat;
   unsigned char bytes[4];
 };
 
+typedef union TwoBytes{
+  uint16_t numDat;
+  unsigned char bytes[2];
+};
+
 // Union instantiations
 FourBytes Packet_Start;     // Byte marker for packet start
-FourBytes LOXLVL;           // LOXLVL sensor data
+TwoBytes LOXLVL;           // LOXLVL sensor data
 FourBytes Packet_End;       // Byte marker for packet start
 
 // Packet definition
-const int SENSOR_MESSAGE_LENGTH = 12;
+const int SENSOR_MESSAGE_LENGTH = 10;
 char SensorDataMessage[SENSOR_MESSAGE_LENGTH];
 
+bool WATER = 1;
 
 void setup() {
   Serial.begin(115200);
@@ -58,49 +68,50 @@ void setup() {
 }
 
 void loop() {
-  time_high = pulseIn(pulse_pin, HIGH, 10000);  // usec
-  time_low = pulseIn(pulse_pin, LOW, 10000);    // usec
+  time_high = pulseIn(pulse_pin, HIGH, 1000000);  // usec
+  time_low = pulseIn(pulse_pin, LOW, 1000000);    // usec
   time_total = time_high + time_low;            // usec
-  LOXLVL.floatDat = (time_total/1e6)/(time_constant*(resistor_one + 2*resistor_two));
+  capacitance = (time_total/1e6)/(time_constant*(resistor_one + 2*resistor_two));
+  // Data interpretation
+  rel_capacitance = capacitance - EMPTY_CAPACITOR_CAPACITANCE;
+  // Capacity as a relative proportion to a fully water-filled capacitor
+  LOXLVL.numDat = rel_capacitance/CAPACITOR_RANGE*100;
 //  Serial.print("HIGH duration: ");
 //  Serial.println(time_high);
 //  Serial.print("LOW duration: ");
 //  Serial.println(time_low);
 //  Serial.print("Capacitance: ");
-//  dtostre(LOXLVL.floatDat, str, 5, 0);
+//  dtostre(capacitance, str, 5, 0);
 //  Serial.print(str);
 //  Serial.println("F");
 //  Serial.println("=================");
 
   ParseWrite_Data();
-//  interrupts();
 //  Debug_Runtime_Print();
-//  noInterrupts();
 }
 
 void Debug_Runtime_Print() {
   // Check the end time
+  interrupts();
   runTime = micros();
   Serial.print("Loop time: ");
   Serial.println((runTime - baseTime)*0.001);
 
   // Check the time
   baseTime = micros();
+  noInterrupts();
 }
 
 void ParseWrite_Data() {
-  // Data interpretation
-  LOXLVL.floatDat = (time_total/1e6)/(time_constant*(resistor_one + 2*resistor_two));
-  
   // Copy the data to the writing array as bytes
-  memcpy(&SensorDataMessage[4],LOXLVL.bytes, 4);
+  memcpy(&SensorDataMessage[4],LOXLVL.bytes, 2);
 
   // Debug prints
 //  Serial.println("============================");
 //  Serial.print("Packet Start Mark: ");
 //  Serial.println(Packet_Start.numDat);
-//  Serial.print("LOX_LVL: ");
-//  Serial.println(LOXLVL.floatDat, 20);
+//  Serial.print("Percent Filled: ");
+//  Serial.println(LOXLVL.numDat);
 //  Serial.print("Packet End Mark: ");
 //  Serial.println(Packet_End.numDat);
 //  Serial.write(Packet_End.bytes,4);
